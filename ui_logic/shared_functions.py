bb58260c081ed515e563e1e7e982c40bb46a7ec0
Salah Khiat <salahkhiat0@gmail.com>
2025-12-08 00:07:58 +0100

    fetch suppliers names and put them into QComboBox

--- ui_logic/shared_functions.py
+++ ui_logic/shared_functions.py
@@ -109,6 +109,9 @@ class SharedFunctions:
             combo.addItem("-")
             
         combo.addItems(items)
+
+    
+
         
     def reverse_dict(self, dictionary:Dict[Any,Any]) -> Dict[Any,Any]:
      """
--- ui_logic/supplier_payment_form.py
+++ ui_logic/supplier_payment_form.py
@@ -1,3 +1,4 @@
+from PyQt6.QtWidgets import QComboBox
 from .base_form import Form
 
 class SupplierPaymentForm(Form):
@@ -5,6 +6,25 @@ class SupplierPaymentForm(Form):
         super().__init__(base_form)
         # set form title
         self.setWindowTitle("إيداع أو دين جديد")
+        # default 
+        self.selected_supplier_id = 0
+
+        # get suppliers
+        suppliers_combo:QComboBox = self.ui.supplier
+        self.fetch_then_put("suppliers","name",suppliers_combo)
+        current_supplier = suppliers_combo.currentText()
+
+        suppliers_combo.currentTextChanged.connect(self.get_current_combo_text)
+
+    
+
+    def get_current_combo_text(self,text:str):
+        """
+            get a supplier id depending on a name
+        """
+        
+        
+
         
 
 
